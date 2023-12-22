a86a11a3a07f5f9ec87cd97d4286a42dcbef38a0
Fayorg <ebr@aketechnology.com>
2023-12-23 00:51:40 +0100

    fix: alarm btn in orange

--- BaseScreen/main.lua
+++ BaseScreen/main.lua
@@ -23,7 +23,7 @@ end
 function alarmButtonExecutor()
     if not redstone.getOutput("top") then
         redstone.setOutput("top", true)
-        buttons[2]:draw(mon, colors.blue)
+        buttons[2]:draw(mon)
     else
         redstone.setOutput("top", false)
         buttons[2]:draw(mon, colors.red)
